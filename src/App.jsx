8608d9445daf94d672f9a7365f4603a4dcfb09ea
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-08-22 01:34:43 -0400

    update a bit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,27 @@ import Player from './components/Player'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [hp1, setHp1] = useState(30);
+  const [hp2, setHp2] = useState(30);
+  
+  useEffect(()=>{
+    let answer = "";
+    if(hp1 <= 0){
+      alert("player2 has won.");
+      answer = prompt('If you wish to replay, type Yes');
+    }
+    else if(hp2 <= 0){
+      alert("player1 has won.");
+      answer = prompt('If you wish to replay, type Yes');
+    }
+
+    if(answer.toLowerCase() == 'yes'){
+      setCount(0);
+      setHp1(30);
+      setHp2(30);
+    }
+  },[hp1,hp2]);
+
   useEffect(()=>{},[count]);
   return (
     <div>
@@ -13,8 +34,8 @@ function App() {
         <h1>Color Battle</h1>
       </header>
       <main>
-        <Player id="0" turn={count} setTurn={setCount}/>
-        <Player id="1" turn={count} setTurn={setCount}/>
+        <Player id="0" turn={count} setTurn={setCount} hp={hp1} enemysHp={hp2} setHp={setHp2}/>
+        <Player id="1" turn={count} setTurn={setCount} hp={hp2} enemysHp={hp1} setHp={setHp1}/>
       </main>
     </div>  
   )
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,12 @@
-export default function Player({id, turn, setTurn}){
+export default function Player({id, turn, setTurn, hp, enemysHp, setHp}){
     function handleTurn(){
         setTurn(++turn);
+        setHp(hp-Math.floor(Math.random() * 10) + 1);
     }
     return(
         <div>
             <h2>{id == 0 ? "Me" : "You"}</h2>
-            <div><h3>HP</h3></div>
+            <div><h3>HP:{hp}</h3></div>
             <div>{turn % 2 == id % 2 ? (<button onClick={()=>handleTurn()}>Attack</button>) : null}</div>
         </div>
     )
